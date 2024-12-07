77de53a3d24bc1f79b62dfb3cfa5fd1062f2852b
Eray Remzi Yıldız <93867237+Lenuxxlacc@users.noreply.github.com>
2024-12-07 15:57:20 +0300

    Update profile_screen.dart

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/core/constants.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -6,6 +7,7 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: arkaplanRengim,
       body: Center(
         child: Text("Profil Ekranim"),
       ),
